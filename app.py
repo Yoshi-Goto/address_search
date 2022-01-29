eeccc553b7c5670638d2d64128a96585a48197e9
Yoshi-Goto <gotech@yahoo.co.jp>
2022-01-29 16:19:19 +0900

    外部ファイル化

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import requests
+from serach_address import search_address
 
 """
 zipcode = "0231133"
@@ -16,20 +16,9 @@ print(a1)
 """
 
 
-def search_address(zipcode):
-    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-    response = requests.get(url)
-    results = response.json()['results']
-    pref_name = results[0]['address1']
-    city_name = results[0]['address2']
-    town_name = results[0]['address3']
-    address = f"{pref_name}{city_name}{town_name}"
-    return address
-
-
 def main():
-    zipcode = "0231133"
-    # zipcode = input("郵便番号<ハイフン無し7桁>は?")   # 郵便番号<ハイフン無し7桁>は?
+    # zipcode = "0231133"
+    zipcode = input("郵便番号<ハイフン無し7桁>は?")  # 郵便番号<ハイフン無し7桁>は?
 
     address = search_address(zipcode)
 
